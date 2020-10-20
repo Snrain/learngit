40e6eee0c99241084551966727378005d125fbbe
林炜康 <weikang.lin@hand-china.com>
2020-10-20 17:43:43 +0800

    aaa

--- .history/src/pages/404_20201020113854.js
+++ .history/src/pages/404_20201020113854.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function() {
   return (
     <div>
-      <h1 style={{ textAlign: 'center' }}>这是404页面</h1>
+      <h1 style={{ textAlign: 'center' }}>出大问题，页面差点不见了!!!</h1>
     </div>
   );
 }
